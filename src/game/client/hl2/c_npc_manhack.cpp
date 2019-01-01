55fe98d374b3c2ea815d039a5e5f1f26167fc660
Sir-Tokialot <34198281+Sir-Tokialot@users.noreply.github.com>
2019-01-02 00:11:51 +0100

    game event when manhack DIE (used for achieveooo)

--- src/game/client/hl2/c_npc_manhack.cpp
+++ src/game/client/hl2/c_npc_manhack.cpp
@@ -178,5 +178,8 @@ void C_NPC_Manhack::SoundShutdown(void)
 		CSoundEnvelopeController::GetController().SoundDestroy( m_pBladeSound );
 		m_pBladeSound = NULL;
 	}
+	IGameEvent *event = gameeventmanager->CreateEvent("manhack_killed"); // call the game event when the manhack dies!
+	if (event)
+		gameeventmanager->FireEvent(event);
 }
 
